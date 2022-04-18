cdf2cff598755b8bd60295c34cc8379f4ca07401
VolodymyrTabarkevych <LRoolzmo357891>
2022-04-18 20:10:12 +0300

    setup main activity, navigation

--- app/src/main/java/com/traday/longholder/MainActivity.kt
+++ app/src/main/java/com/traday/longholder/MainActivity.kt
@@ -2,11 +2,32 @@ package com.traday.longholder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.traday.longholder.databinding.ActivityMainBinding
+import com.traday.longholder.extensions.gone
+import com.traday.longholder.extensions.hide
+import com.traday.longholder.extensions.show
+import com.traday.longholder.presentation.base.TabBarHandler
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main), TabBarHandler {
+
+    private val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::bind)
+    private val navHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.fcvMain) as NavHostFragment }
+    private val navController by lazy { navHostFragment.navController }
+    private val rootGraph by lazy { navController.navInflater.inflate(R.navigation.nav_main) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    }
+
+    override fun showTabs() {
+        binding.bnvMain.show()
+    }
+
+    override fun hideTabs() {
+        binding.bnvMain.gone()
     }
 }
