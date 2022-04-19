6b17a96093f5450ad65a27f1d55bda2ddac04ba0
VolodymyrTabarkevych <LRoolzmo357891>
2022-04-19 14:31:24 +0300

    update welcome screen with new widgets and fonts

--- app/src/main/java/com/traday/longholder/presentation/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/traday/longholder/presentation/welcome/WelcomeFragment.kt
@@ -2,24 +2,20 @@ package com.traday.longholder.presentation.welcome
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.traday.longholder.R
 import com.traday.longholder.databinding.FragmentWelcomeBinding
-import com.traday.longholder.presentation.base.BaseMVVMFragment
+import com.traday.longholder.presentation.base.BaseFragment
 
-class WelcomeFragment : BaseMVVMFragment<WelcomeViewModel, FragmentWelcomeBinding>(
+class WelcomeFragment : BaseFragment<FragmentWelcomeBinding>(
     R.layout.fragment_welcome
 ) {
 
     override val binding: FragmentWelcomeBinding by viewBinding(FragmentWelcomeBinding::bind)
 
-    override val viewModel: WelcomeViewModel by viewModels()
-
     override fun initView(inflatedView: View, args: Bundle?) {
     }
 
-    override fun initViewModel() {
-    }
+    override fun initViewModel() {}
 
 }
--- app/src/main/java/com/traday/longholder/presentation/welcome/WelcomeViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.traday.longholder.presentation.welcome
-
-import com.traday.longholder.presentation.base.BaseViewModel
-
-class WelcomeViewModel : BaseViewModel() {
-}
