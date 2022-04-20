c6fcfbe7995975dacf3e46c9f6d031b876c31318
VolodymyrTabarkevych <LRoolzmo357891>
2022-04-20 15:23:53 +0300

    add profile screen

---
+++ app/src/main/java/com/traday/longholder/presentation/profile/ProfileFragment.kt
@@ -0,0 +1,38 @@
+package com.traday.longholder.presentation.profile
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.traday.longholder.R
+import com.traday.longholder.databinding.FragmentProfileBinding
+import com.traday.longholder.presentation.base.BaseMVVMFragment
+import com.traday.longholder.presentation.base.StatusBarMode
+import com.traday.longholder.presentation.base.TabBarMode
+
+class ProfileFragment : BaseMVVMFragment<ProfileViewModel, FragmentProfileBinding>(
+    layoutResId = R.layout.fragment_profile,
+    statusBarMode = StatusBarMode.Secondary,
+    tabBarMode = TabBarMode.VISIBLE
+) {
+
+    override val binding: FragmentProfileBinding by viewBinding(FragmentProfileBinding::bind)
+
+    override val viewModel: ProfileViewModel by viewModels()
+
+    override fun initView(inflatedView: View, args: Bundle?) {
+        initActionButtons()
+    }
+
+    private fun initActionButtons() {
+        with(binding) {
+            flProfileNotifications.setOnClickListener {}
+            flProfileLanguage.setOnClickListener {}
+            flProfileTheme.setOnClickListener {}
+            flProfileSubscription.setOnClickListener {}
+            flProfileLogout.setOnClickListener {}
+        }
+    }
+
+    override fun initViewModel() {}
+}
