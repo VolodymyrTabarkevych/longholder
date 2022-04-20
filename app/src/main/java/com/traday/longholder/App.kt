df0bd4d9abb6733a3e90dd1f4c44e939295207b2
VolodymyrTabarkevych <LRoolzmo357891>
2022-04-20 13:06:06 +0300

    disable dark mode

--- app/src/main/java/com/traday/longholder/App.kt
+++ app/src/main/java/com/traday/longholder/App.kt
@@ -1,7 +1,14 @@
 package com.traday.longholder
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
