132c69b18ef1ab6cc6d357154bbd295d18ec6d81
VolodymyrTabarkevych <LRoolzmo357891>
2022-04-22 09:33:22 +0300

    improve clear focus edit text widget

--- app/src/main/java/com/traday/longholder/presentation/customviews/ClearFocusTextInputEditText.kt
+++ app/src/main/java/com/traday/longholder/presentation/customviews/ClearFocusTextInputEditText.kt
@@ -3,6 +3,7 @@ package com.traday.longholder.presentation.customviews
 import android.content.Context
 import android.util.AttributeSet
 import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
 import com.google.android.material.textfield.TextInputEditText
 
 class ClearFocusTextInputEditText @JvmOverloads constructor(
@@ -11,10 +12,15 @@ class ClearFocusTextInputEditText @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : TextInputEditText(context, attrs, defStyleAttr) {
 
+    override fun onEditorAction(actionCode: Int) {
+        super.onEditorAction(actionCode)
+        if (actionCode == EditorInfo.IME_ACTION_DONE) {
+            clearFocus()
+        }
+    }
+
     override fun onKeyPreIme(keyCode: Int, event: KeyEvent): Boolean {
-        if (keyCode == KeyEvent.KEYCODE_BACK &&
-            event.action == KeyEvent.ACTION_UP
-        ) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.action == KeyEvent.ACTION_UP) {
             clearFocus()
             return false
         }
