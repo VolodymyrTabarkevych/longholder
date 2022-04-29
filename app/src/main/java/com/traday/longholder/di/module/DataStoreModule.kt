61639e74149b4e0c4568d0cc89d121328631d09d
VolodymyrTabarkevych <LRoolzmo357891>
2022-04-29 14:27:27 +0300

    add user data store

---
+++ app/src/main/java/com/traday/longholder/di/module/DataStoreModule.kt
@@ -0,0 +1,41 @@
+package com.traday.longholder.di.module
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.SharedPreferencesMigration
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import com.traday.longholder.di.qualifire.UserPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    private const val USER_PREFERENCES_NAME = "user_preferences"
+
+    @Singleton
+    @Provides
+    @UserPreferences
+    fun provideUserPreferencesDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler(
+                produceNewData = { emptyPreferences() }),
+            migrations = listOf(SharedPreferencesMigration(context, USER_PREFERENCES_NAME)),
+            scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+            produceFile = { context.preferencesDataStoreFile(USER_PREFERENCES_NAME) }
+        )
+    }
+}
