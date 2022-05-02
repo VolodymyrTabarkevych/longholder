f085ae518ed08c3801259bbd5642d9f1d4eb6329
VolodymyrTabarkevych <LRoolzmo357891>
2022-05-02 08:43:55 +0300

    refactor

--- app/src/main/java/com/traday/longholder/data/local/database/dao/UserDao.kt
+++ app/src/main/java/com/traday/longholder/data/local/database/dao/UserDao.kt
@@ -3,13 +3,14 @@ package com.traday.longholder.data.local.database.dao
 import androidx.room.Dao
 import androidx.room.Query
 import com.traday.longholder.data.local.entity.UserEntity
+import com.traday.longholder.data.local.entity.UserEntity.Companion.TABLE_NAME
 
 @Dao
 interface UserDao : BaseDao<UserEntity> {
 
-    @Query("SELECT * FROM user LIMIT 1")
+    @Query("SELECT * FROM $TABLE_NAME LIMIT 1")
     suspend fun getUser(): UserEntity
 
-    @Query("DELETE FROM user")
+    @Query("DELETE FROM $TABLE_NAME")
     suspend fun deleteUser()
 }
--- app/src/main/java/com/traday/longholder/data/local/entity/UserEntity.kt
+++ app/src/main/java/com/traday/longholder/data/local/entity/UserEntity.kt
@@ -3,8 +3,9 @@ package com.traday.longholder.data.local.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.traday.longholder.data.local.entity.UserEntity.Companion.TABLE_NAME
 
-@Entity(tableName = "user")
+@Entity(tableName = TABLE_NAME)
 data class UserEntity(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id") val id: Int = 0,
@@ -14,4 +15,10 @@ data class UserEntity(
     @Embedded(prefix = "crypto") val cryptos: List<CryptoEntity>,
     @Embedded(prefix = "message") val messages: List<MessageEntity>,
     @Embedded(prefix = "report") val reports: List<ReportEntity>*/
-)
+) {
+
+    companion object {
+
+        const val TABLE_NAME = "user"
+    }
+}
